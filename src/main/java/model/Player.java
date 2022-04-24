caa17d1db58456afbe9305fa153e341d2f090fd6
luis <luisfelorivas@gmai.com>
2022-04-23 23:07:53 -0500

    upadte retoSofkaJavaMaven project: the connection with the database were added.

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -9,14 +9,33 @@ import model.interfaces.IPlayer;
  * @since 1.0.0 2022-04-22.
  */
 public class Player extends  Person implements IPlayer {
+    private Integer score;
+
+
+    public Player(){}
+
+
     @Override
-    public void setName() {
+    public void setName(String name) {
+        this.name=name;
 
     }
 
 
-    @Override
-    public void getId() {
+    public Integer getId() {
+        return id;
 
+    }
+    public void setId(Integer id) {
+        this.id= this.id;
+
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
     }
 }
