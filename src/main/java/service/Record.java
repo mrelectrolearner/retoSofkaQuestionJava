4feaa2257591dbf684211340f1b723eecd31ecfc
mrelectrolearner <78055368+mrelectrolearner@users.noreply.github.com>
2022-04-24 23:49:54 -0500

    Update Record.java
    add documentation

--- src/main/java/service/Record.java
+++ src/main/java/service/Record.java
@@ -7,6 +7,12 @@ import javax.swing.JOptionPane;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+/**
+ *Represent the record service use in the program to get and save the winners of the game.
+ * @version 1.0.0 2022-04-22.
+ * @author
+ * @since 1.0.0 2022-04-22.
+ */
 public class Record implements IRecord {
     @Override
     public void saveRecord(Player player) throws SQLException {
