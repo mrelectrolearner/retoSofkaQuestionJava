78c89b0859bae94d96d0f547c917a40c114d574d
mrelectrolearner <78055368+mrelectrolearner@users.noreply.github.com>
2022-04-24 23:41:36 -0500

    Update Question.java
    add class documentation

--- src/main/java/model/Question.java
+++ src/main/java/model/Question.java
@@ -1,5 +1,11 @@
 package model;
 
+/**
+ *Represent the questions use in the program.
+ * @version 1.0.0 2022-04-22.
+ * @author
+ * @since 1.0.0 2022-04-22.
+ */
 public class Question extends  Information{
 
     public Question(String question) {
